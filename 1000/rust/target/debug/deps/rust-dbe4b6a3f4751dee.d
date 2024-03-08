3701148069c9252d33a5bc4f7b485be8c1bdbbf7
Gustavo Teixeira <contact@gustavoteixeira.dev>
2024-03-08 00:37:18 -0300

    removed target folder

--- 1000/rust/target/debug/deps/rust-dbe4b6a3f4751dee.d
+++
@@ -1,5 +0,0 @@
-/home/teixeira/personal/beecrowd/rust/target/debug/deps/rust-dbe4b6a3f4751dee: src/main.rs
-
-/home/teixeira/personal/beecrowd/rust/target/debug/deps/rust-dbe4b6a3f4751dee.d: src/main.rs
-
-src/main.rs:
--- 1000/rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-/home/teixeira/personal/beecrowd/rust/target/debug/rust: /home/teixeira/personal/beecrowd/rust/src/main.rs
